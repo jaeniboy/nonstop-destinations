0aacb5f87b1697e333aadb65ad360de6ed7b4645
Jan Seipel <janfseipel@gmail.com>
2024-11-17 21:51:04 +0100

    Moved backend dependencies to backend directory

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,7 @@ app.get('/destinations', async (req, res) => {
   //http://localhost:3000/destinations?station=8000191&radius=2000
   const stationId = req.query.station
   const radius = req.query.radius
+  // todo: variable time value
   const time = "2024-11-09 08:00"
   const nonStopStations = await getAllNonStopStations(stationId, time)
   const enhancedStations = await enhancedStopovers(Object.values(nonStopStations), radius)
