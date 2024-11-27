f211aac108af38924923fb561db30dc27a425bac
Jan Seipel <janfseipel@gmail.com>
2024-11-27 20:30:47 +0100

    styling improvements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,8 @@ import Destinations from './Destinations';
 import Map from "./Map";
 import SuggestionTitleBox from './SuggestionTitleBox';
 import SuggestionInfoBox from './SuggestionInfoBox';
-import logo from './assets/nsd_logo.svg'
+// import logo from './assets/nsd_logo.svg'
+import logo from './assets/nsd_logo_all_path_white.svg'
 
 function App() {
 
@@ -27,28 +28,30 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col justify-center">
-        <div className="w-full flex justify-center text-center mt-8">
-          <img src={logo} />
+      <div className="flex flex-col justify-center h-full">
+        <div>
+          <div className="w-full flex justify-center text-center pt-5 pb-10 bg-indigo-700 rounded-b-3xl">
+            <img src={logo} className="h-16" />
+          </div>
         </div>
-        <div className="w-full flex justify-center mt-8">
-          <StationSearch sendStations={sendStations} />
-        </div>
+          <div className="w-full flex justify-center relative -translate-y-1/4">
+            <StationSearch sendStations={sendStations} />
+          </div>
 
 
         {stations.length != 0 &&
           <>
-            <SuggestionTitleBox 
-              stationName={station.name} 
+            <SuggestionTitleBox
+              stationName={station.name}
               index={stationDisplayIndex}
               nextStation={nextStation}
               previousStation={previousStation}
-              lastSuggestion={stations.length}/>
+              lastSuggestion={stations.length} />
             <div class="w-full aspect-square md:w-1/2 lg:w-3/10 p-4">
-              <Map station={station}/>
+              <Map station={station} />
             </div>
             <div>
-              <SuggestionInfoBox data={station}/>
+              <SuggestionInfoBox data={station} />
             </div>
           </>
         }
--- frontend/src/SuggestionTitleBox.jsx
+++ frontend/src/SuggestionTitleBox.jsx
@@ -7,7 +7,7 @@ const SuggestionTitleBox = ({ stationName, index, nextStation, previousStation, 
     const suggestionNumber = index + 1
     return (
         <IconContext.Provider value={{ className: "text-2xl text-gray-400" }}>
-            <div className="flex justify-between px-5 mt-5 items-center">
+            <div className="flex justify-between px-5 items-center">
                 <button onClick={previousStation} className={index <= 0 && "invisible"} >
                     <BsChevronLeft value={{ color: "blue" }} />
                 </button>
